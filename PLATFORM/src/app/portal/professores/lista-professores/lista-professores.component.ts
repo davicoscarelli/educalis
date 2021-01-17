5ed77484af02713f39cd8ceccfdd0a107025899d
Juuh Aintablian <juuh.ian@gmail.com>
2021-01-17 00:05:45 -0300

    professores

--- PLATFORM/src/app/portal/professores/lista-professores/lista-professores.component.ts
+++ PLATFORM/src/app/portal/professores/lista-professores/lista-professores.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  faBookReader
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-lista-professores',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProfessoresComponent implements OnInit {
 
-  constructor() { }
+  faBookReader = faBookReader;
+
+  professores = [
+    {
+      label: 'Geografia',
+      urlImage: '../../../../assets/img/professores/geral/prof-geografia.png',
+      professor: 'David Brasil',
+      id: 'geo',
+    },
+    {
+      label: 'Matemática',
+      urlImage: '../../../../assets/img/professores/geral/prof-matematica.png',
+      professor: 'Juliana Ainb',
+      id: 'mat',
+    },
+    {
+      label: 'Física',
+      urlImage: '../../../../assets/img/professores/geral/prof-fisica.png',
+      professor: 'Albert Junior',
+      id: 'fis',
+    },
+    {
+      label: 'Química',
+      urlImage: '../../../../assets/img/professores/geral/prof-quimica.png',
+      professor: 'Carla Nunes',
+      id: 'quim',
+    },
+  ];
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onClickCard(id) {
+    switch (id) {
+      case 'mat':
+        this.route.navigate(['portal/professores/matematica']);
+        break;
+      default:
+        console.log('Ainda não emplementado :(');
+    }
   }
 
 }
--- PLATFORM/src/app/portal/professores/professores-matematica/professores-matematica.component.ts
+++ PLATFORM/src/app/portal/professores/professores-matematica/professores-matematica.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  faBookReader
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-professores-matematica',
@@ -6,8 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./professores-matematica.component.css']
 })
 export class ProfessoresMatematicaComponent implements OnInit {
+  faBookReader = faBookReader;
 
-  constructor() { }
+  professores = [
+    {
+      label: 'Matemática',
+      urlImage: '../../../../assets/img/professores/matematica/prof-1.png',
+      professor: 'Juliana Ainb',
+    },
+    {
+      label: 'Matemática',
+      urlImage: '../../../../assets/img/professores/matematica/prof-2.png',
+      professor: 'Maria Eugenia',
+    },
+    {
+      label: 'Matemática',
+      urlImage: '../../../../assets/img/professores/matematica/prof-3.png',
+      professor: 'Clotilde Costa',
+    },
+    {
+      label: 'Matemática',
+      urlImage: '../../../../assets/img/professores/matematica/prof-4.png',
+      professor: 'Julia Mara',
+    },
+  ];
+
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
